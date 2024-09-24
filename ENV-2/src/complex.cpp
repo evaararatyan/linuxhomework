f36dc675821b3af571eb8b68de44dbc4fb5de59b
evaararatyan <eva.araratyan@student.rau.am>
2024-09-24 23:54:45 +0400

    Update complex.cpp

--- ENV-2/src/complex.cpp
+++ ENV-2/src/complex.cpp
@@ -1,24 +1,30 @@
 #include "complex.h"
 #include <cmath>
 
+// Constructor: initializes a Complex number
  Complex::Complex(double r, double i) : real(r), imag(i) {}
 
+// Operator overload: adds two Complex numbers
  Complex Complex::operator+(const Complex& other) const {
          return Complex(real + other.real, imag + other.imag);
  }
 
+// Operator overload: takes away one Complex num from another.
  Complex Complex::operator-(const Complex& other) const {
          return Complex(real - other.real, imag - other.imag);
  }
 
+//Operator overload: multiplies by scalar
  Complex Complex::operator*(double scalar) const {
          return Complex(real * scalar, imag * scalar);
  }
 
+ //Returns the length of Complex num
  double Complex::abs() const {
          return std::sqrt(real * real + imag * imag);
  }
 
+// Prints the Complex numb
 void Complex::print() const { 
         std::cout << "(" << real << "," << imag << "i)" << std::endl;
 }
