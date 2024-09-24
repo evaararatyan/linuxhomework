0f2c104e907692922b47f7a6071109bcf9487d15
evaararatyan <eva.araratyan@student.rau.am>
2024-09-24 23:59:58 +0400

    Update sort.cpp

--- ENV-2/src/sort.cpp
+++ ENV-2/src/sort.cpp
@@ -1,10 +1,12 @@
 #include "sort.h"
 #include <algorithm>
 
+// Compare two complex numbers
 bool compareComplex(const Complex& a, const Complex& b) {
     return a.abs() < b.abs();
 }
 
+// Sort array of complex numbs
 void sortComplex(std::vector<Complex>& arr) {
     std::sort(arr.begin(), arr.end(), compareComplex);
 }
