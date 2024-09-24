0bf2e15e7b6ff64efef95f36699f3e346286a754
evaararatyan <eva.araratyan@student.rau.am>
2024-09-24 23:57:54 +0400

    Update main.cpp

--- ENV-2/src/main.cpp
+++ ENV-2/src/main.cpp
@@ -3,17 +3,21 @@
 #include "sort.h"
 
 int main() {
+	// Create a vector of complex nums
 	std::vector<Complex> numbers = {
 		Complex(1, 2), Complex(3, 4), Complex(2, 2), Complex(0, 5)
 	};
-
+	
+        // Print numbers before sorting
 	std::cout << "Before sorting:" << std::endl;
 	for (const auto& num : numbers) {
 		num.print();
 	}
 	
+	// Sort complex numbers
 	sortComplex(numbers);
 
+	// Print numbers after sorting
 	std::cout << "After sorting:" << std::endl;
 	for (const auto& num : numbers) {
 		num.print();
